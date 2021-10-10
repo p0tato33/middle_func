dc7f2fe628a473105c39dd61d041e8fb9cb479f9
p0tato33 <67389389+p0tato33@users.noreply.github.com>
2021-10-11 01:19:59 +0300

    Create middle.h

---
+++ middle.h
@@ -0,0 +1,29 @@
+#ifndef MIDDLE_H
+#define MIDDLE_H
+
+#include <iostream>
+using namespace std;
+
+void itc_num_print(int num); 
+int itc_len_num(long long num); 
+int itc_multi_num(long long num);
+int itc_max_num(long long num); 
+int itc_rev_num(long long num);
+int itc_null_count(long long num); 
+bool itc_mirror_num(long long num); 
+bool itc_mirror_count(long long num);
+int itc_second_simple_max_num(long long num); 
+long long itc_bin_num(long long num); 
+long long itc_oct_num(long long num);
+int itc_rev_bin_num(long long num);
+int itc_rev_oct_num(long long num); 
+int itc_sum_num(long long num); 
+int itc_second_max_num(long long num); 
+int itc_min_num(long long num); 
+long long itc_abs(long long num1);
+long long itc_pow(long long num1, long long num2);
+
+int itc_rev_covert_num(long long num, int num2);
+
+
+#endif // MIDDLE_H
