1e6d2b421300359cde5b2b82bff049310763c12e
p0tato33 <67389389+p0tato33@users.noreply.github.com>
2021-10-15 00:50:14 +0300

    Update middle.h

--- middle.h
+++ middle.h
@@ -4,22 +4,23 @@
 #include <iostream>
 using namespace std;
 
-void itc_num_print(int num); 
-int itc_len_num(long long num); 
-long long itc_multi_num(long long num);
-int itc_max_num(long long num); 
-int itc_rev_num(long long num);
-int itc_null_count(long long num); 
-bool itc_mirror_num(long long num); 
-int itc_mirror_count(long long num);
-int itc_second_simple_max_num(long long num); 
-long long itc_bin_num(long long num); 
-long long itc_oct_num(long long num);
-int itc_rev_bin_num(long long num);
-int itc_rev_oct_num(long long num); 
-int itc_sum_num(long long num); 
+void itc_num_print(int num);       // 1
+int itc_len_num(long long num);    // 2
+int itc_sum_num(long long num);    // 3
+long long itc_multi_num(long long num);  // 4
+int itc_max_num(long long num);    // 5
+int itc_min_num(long long num);    // 6
+int itc_rev_num(long long num);    // 7
+int itc_null_count(long long num); // 8
+bool itc_mirror_num(long long num); // 9
+int itc_mirror_count(long long num);  // 10
+int itc_second_max_num(long long num); // 11
+int itc_second_simple_max_num(long long num); // 12
+long long itc_bin_num(long long num);  // 13
+long long itc_oct_num(long long num);  // 14
+int itc_rev_bin_num(long long num);   // 15
+int itc_rev_oct_num(long long num);   // 16
 int itc_second_max_num(long long num); 
-int itc_min_num(long long num); 
 long long itc_abs(long long num1);
 long long itc_pow(long long num1, long long num2);
 
