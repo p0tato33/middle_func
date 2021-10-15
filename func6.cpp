318cbb9ff1efb9d1037f6720bbf4a1606bb7c30f
p0tato33 <67389389+p0tato33@users.noreply.github.com>
2021-10-15 10:27:58 +0300

    Update func6.cpp

--- func6.cpp
+++ func6.cpp
@@ -1,18 +1,18 @@
 #include "middle.h"
 
-int itc_second_max_num(long long num) {
-	long long num1 = itc_abs(num);
-	int max1 = num1 % 10;
-	int max2 = 0;
-	for (int n1 = itc_len_num(num1); n1 > 0; n1--) {
-		if (num1 % itc_pow(10, n1) / itc_pow(10, n1 - 1) > max1)
-			max1 = num1 % itc_pow(10, n1) / itc_pow(10, n1 - 1);
-		else if ((num1 % itc_pow(10, n1) / itc_pow(10, n1 - 1) < max1) && (num1 % itc_pow(10, n1) / itc_pow(10, n1 - 1) > max2))
-			max2 = num1 % itc_pow(10, n1) / itc_pow(10, n1 - 1);
-	}
-	if (max1 == itc_sum_num(num) / itc_len_num(num))
-		return max1;
-	return max2;
+int itc_second_max_num(long long num)
+{
+    num = itc_abs(num);
+    int last = itc_min_num(num);
+    int maxx = itc_max_num(num);
+    while(num != 0)
+    {
+        if(num & 10 > last && num % 10 < maxx)
+            last = num % 10;
+        num /= 10;
+        
+    }
+    return last;
 }
 
 int itc_second_simple_max_num(long long num) {
