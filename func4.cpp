4323273d6dfa49a65934386e0a09b23395a03e4c
p0tato33 <67389389+p0tato33@users.noreply.github.com>
2021-10-15 09:52:22 +0300

    Update func4.cpp

--- func4.cpp
+++ func4.cpp
@@ -1,16 +1,19 @@
 #include "middle.h"
 
-int itc_rev_num(long long num) { // 7
-	long long num3 = itc_abs(num);
-	long long num1 = 0;
-	long counter = 0;
-	for (int n1 = itc_len_num(num3); n1 > 0; n1--) {
-		num1 += (num3 % itc_pow(10, itc_len_num(num3) - n1 + 1) / itc_pow(10, itc_len_num(num3) - n1) * itc_pow(10, n1 - 1));
-	}
-	for (int n1 = itc_len_num(num1); n1 > 0; n1--) {
-		counter += 1;
-	}
-	return counter;
+int itc_rev_num(long long num)
+{
+    if(num==0)
+    {
+        return 1;
+    }
+    int counter=num%10;
+    while( counter == 0)
+    {
+        num=num/10;
+        counter=num%10;
+
+    }
+    return(itc_len_num(num));
 }
 
 int itc_null_count(long long num) { // 8
