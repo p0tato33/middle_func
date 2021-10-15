7f5bbfb2c8483c68ae64a9236d113588e8646026
p0tato33 <67389389+p0tato33@users.noreply.github.com>
2021-10-15 09:48:47 +0300

    Update func3.cpp

--- func3.cpp
+++ func3.cpp
@@ -10,11 +10,23 @@ int itc_max_num(long long num) {
 	return result;
 }
 
-int itc_min_num(long long num) { 
-	int result = num % 10;
-	for (int n1 = itc_len_num(num); n1 > 0; n1--) {
-		if (num % itc_pow(10, n1) / itc_pow(10, n1 - 1) < result)
-			result = num % itc_pow(10, n1) / itc_pow(10, n1 - 1);
-	}
-	return result;
+int itc_min_num(long long num)
+{
+    if(num<0)
+    {
+        num=num*(-1);
+    }
+    long long minnum=num%10;
+    long long counter;
+    int len=itc_len_num(num);
+    for(int i=0;i<len;i++)
+    {
+        counter=num%10;
+        if(counter<minnum)
+        {
+            minnum=counter;
+        }
+        num=num/10;
+    }
+    return(int(minnum));
 }
